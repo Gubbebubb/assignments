7c415807afcc6db9c816d47e77842371feeb5cb5
Gabriel Elfström <ge222ik@student.lnu.se>
2026-02-08 23:45:35 +0100

    tryy

--- assignments.js
+++ assignments.js
@@ -1,1 +1,1 @@
-export const asgn = [{id: "asgn1", title: "Uppgift 1", link: "../assignments/assignment1/", description: "Uppgift 1 handlade om att skapa denna sida. Vad meta... "}, {id: "asgn2", title: "Uppgift 2", link: "../assignments/assignment2/", description: "Uppgift 2 ska jag snart göra... "}]
+export const asgn = [{id: "asgn1", title: "Uppgift 1", link: "assignment1/", description: "Uppgift 1 handlade om att skapa denna sida. Vad meta... "}, {id: "asgn2", title: "Uppgift 2", link: "assignment2/", description: "Uppgift 2 ska jag snart göra... "}]
