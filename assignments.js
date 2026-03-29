0922de3cb74a182b5f3446341d532301daab38e7
Gabriel Elfström <ge222ik@student.lnu.se>
2026-03-29 01:33:10 +0100

    landningssida tillagd

--- assignments.js
+++ assignments.js
@@ -1,3 +1,3 @@
-export const asgn = [{id: "asgn1", title: "Uppgift 1", link: "assignment1/", description: "Uppgift 1 handlade om att skapa denna sida. Vad meta... "}, {id: "asgn2", title: "Uppgift 2", link: "assignment2/", description: "Uppgift 2 ska jag snart göra... "}, {id: "asgn3", title: "Uppgift 3", link: "assignment3/", description: "Uppgift 3 handlar om att dynamiskt skapa en turneringssimulering! "}]
+export const asgn = [{id: "asgn1", title: "Uppgift 1", link: "assignment1/", description: "Uppgift 1 handlade om att skapa denna sida. Vad meta... "}, {id: "asgn2", title: "Uppgift 2", link: "assignment2/", description: "Uppgift 2 handlar om en inte-så-kass kassa funktion! "}, {id: "asgn3", title: "Uppgift 3", link: "assignment3/", description: "Uppgift 3 handlar om att dynamiskt skapa en turneringssimulering! "}, {id: "asgn4", title: "Uppgift 4", link: "assignment4/", description: "Uppgift 4 handlar om världens kusligaste spökhusbyrå! "}]
 
 //github push?
